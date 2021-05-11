61e3ffd985bc20275b0ee31396a81f5adc1beadf
Daniel Douglas <dan.douglas.01@gmail.com>
2021-05-11 13:38:52 +0100

    task(footer): copyright

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  date: number;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.date = new Date().getFullYear();
   }
 
 }
