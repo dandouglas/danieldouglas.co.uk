3af813b58d44d9d72b36a6f06330f8d7750944bd
Daniel Douglas <dan.douglas.01@gmail.com>
2021-05-11 20:36:11 +0100

    task(about): hide read more link

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Router, Event as NavigationEvent, NavigationStart } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnDestroy {
+  isAboutPage: boolean;
+  destroy$ = new Subject<void>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private router: Router
+  ) {
+    this.router.events
+    .pipe(takeUntil(this.destroy$))
+    .subscribe((event: NavigationEvent) => {
+      if (event instanceof NavigationStart) {
+        this.isAboutPage = event.url === '/about'
+      }
+    });
   }
 
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
