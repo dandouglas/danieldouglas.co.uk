454d6e2715aa7b29d936d3f12a42b3ab782cab6f
Daniel Douglas <dan.douglas.01@gmail.com>
2021-05-12 11:41:51 +0100

    task(home): latest posts section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private scully: ScullyRoutesService) {}
+  posts$: Observable<ScullyRoute[]>;
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.posts$ = this.scully.available$.pipe(
+      map(routeList => {
+        return routeList.filter((route: ScullyRoute) =>
+          route.route.startsWith(`/blog/`)
+        );
+      })
+    );
   }
 
 }
