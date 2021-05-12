00829e12540cdcebd349454ad051516cf07131f6
Daniel Douglas <dan.douglas.01@gmail.com>
2021-05-12 15:05:46 +0100

    task(app): styling

--- src/app/shared/blog-card/blog-card.component.ts
+++ src/app/shared/blog-card/blog-card.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-blog-card',
   templateUrl: './blog-card.component.html',
-  styleUrls: ['./blog-card.component.scss']
+  styleUrls: ['./blog-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BlogCardComponent implements OnInit {
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { BlogCardComponent } from './blog-card/blog-card.component';
 import { FancyLinkComponent } from './fancy-link/fancy-link.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -10,6 +11,7 @@ import { FancyLinkComponent } from './fancy-link/fancy-link.component';
   exports: [BlogCardComponent, FancyLinkComponent],
   imports: [
     CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
