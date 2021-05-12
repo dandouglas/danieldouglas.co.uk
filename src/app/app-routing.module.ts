d19ee5e12eabc5dbf6e2f5adb4567f91ae95f02f
Daniel Douglas <dan.douglas.01@gmail.com>
2021-05-12 15:36:30 +0100

    fix(header): active links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
   },
   { path: 'blog',
