ba3ae3796fe383c1370e7be3bb2e3564336b684f
Daniel Douglas <dan.douglas.01@gmail.com>
2021-05-13 20:11:22 +0100

    task(about): add images

--- src/app/pages/about/about.module.ts
+++ src/app/pages/about/about.module.ts
@@ -1,10 +1,11 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
 
 @NgModule({
   declarations: [AboutComponent],
-  imports: [CommonModule, AboutRoutingModule],
+  imports: [CommonModule, AboutRoutingModule, SharedModule],
 })
 export class AboutModule {}
