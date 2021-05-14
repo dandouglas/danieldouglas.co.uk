7d3cfc3449f431762f82374942a535740ede267a
Daniel Douglas <dan.douglas.01@gmail.com>
2021-05-14 10:02:07 +0100

    task(footer): social icons

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faStackOverflow, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  readonly faStackOverflow = faStackOverflow;
+  readonly faLinkedin = faLinkedin;
+  readonly faGithub = faGithub;
+  readonly faEnvelope = faEnvelope;
   date: number;
 
   constructor() { }
