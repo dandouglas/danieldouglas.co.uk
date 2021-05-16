0ae47d59a921114c153ba4ab6703dcbde00682d7
Daniel Douglas <dan.douglas.01@gmail.com>
2021-05-16 11:51:57 +0100

    task(header): hide side menu after nav event

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,23 +1,43 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { NavigationStart, Router, Event as NavigationEvent } from '@angular/router';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnDestroy {
 
   readonly faBars = faBars;
   readonly faTimes = faTimes;
   sideNavIsVisible = false;
   @ViewChild('nav') nav: ElementRef;
+  destroy$ = new Subject<void>();
 
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) {
+    this.router.events
+    .pipe(takeUntil(this.destroy$))
+    .subscribe((event: NavigationEvent) => {
+      if (event instanceof NavigationStart) {
+        this.nav.nativeElement.classList.remove('nav-active');
+        this.sideNavIsVisible = false;
+      }
+    });
+  }
 
-  toggleNav() {
+  toggleNav(): void {
     this.nav.nativeElement.classList.toggle('nav-active');
     this.sideNavIsVisible = !this.sideNavIsVisible;
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
 }
