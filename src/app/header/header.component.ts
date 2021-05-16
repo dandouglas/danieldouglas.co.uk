a88fa58863ed11886e34e12d19966ef95e49d379
Daniel Douglas <dan.douglas.01@gmail.com>
2021-05-16 11:46:44 +0100

    task(header): angularise!

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,35 +1,23 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements AfterViewInit {
+export class HeaderComponent {
 
   readonly faBars = faBars;
   readonly faTimes = faTimes;
-  nav: any;
-  burger: any;
   sideNavIsVisible = false;
+  @ViewChild('nav') nav: ElementRef;
 
 
   constructor() { }
 
-  ngAfterViewInit(): void {
-  // Selection of HTML objects
-  this.burger = document.querySelector('.burger');
-  this.nav = document.querySelector('.nav');
-  }
-
-
-
-// Defining a function
-toggleNav() {
-    // this.burger.classList.toggle('fa-bars');
-    // this.burger.classList.toggle('fa-times');
-    this.nav.classList.toggle('nav-active');
+  toggleNav() {
+    this.nav.nativeElement.classList.toggle('nav-active');
     this.sideNavIsVisible = !this.sideNavIsVisible;
-}
+  }
 
 }
