1a46f8e92f98380471f58edea70c692a64658a6f
Daniel Douglas <dan.douglas.01@gmail.com>
2021-05-16 10:47:18 +0100

    task(header): responsive nav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements AfterViewInit {
+
+  readonly faBars = faBars;
+  nav: any;
+  burger: any;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+  // Selection of HTML objects
+  this.burger = document.querySelector('.burger');
+  this.nav = document.querySelector('.nav');
   }
+
+
+
+// Defining a function
+toggleNav() {
+    // this.burger.classList.toggle('fa-bars');
+    // this.burger.classList.toggle('fa-times');
+    this.nav.classList.toggle('nav-active');
+}
 
 }
