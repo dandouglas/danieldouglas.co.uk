6cdbdb99f155dcf614defdabdba0433e00f3ecd3
Daniel Douglas <dan.douglas.01@gmail.com>
2021-06-10 21:28:10 +0100

    chore(ga): add analytics

--- scully.danieldouglas.config.ts
+++ scully.danieldouglas.config.ts
@@ -1,9 +1,23 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { setPluginConfig, ScullyConfig, prod } from '@scullyio/scully';
+import { GoogleAnalytics } from '@scullyio/scully-plugin-google-analytics';
+
+const defaultPostRenderers = [];
+
+if (prod) {
+  setPluginConfig(GoogleAnalytics, { globalSiteTag: 'G-K3PYY83029' });
+  defaultPostRenderers.push(GoogleAnalytics);
+}
+
 export const config: ScullyConfig = {
+  defaultPostRenderers,
   projectRoot: "./src",
   projectName: "danieldouglas",
   outDir: './dist/static',
   routes: {
+    '/': {
+      type: 'contentFolder',
+      postRenderers: [...defaultPostRenderers],
+    },
     '/blog/:slug': {
       type: 'contentFolder',
       slug: {
