1c630b3136c8903783cc46295ced9add03650580
Daniel Douglas <dan.douglas.01@gmail.com>
2021-09-26 12:28:55 +0100

    chore(app): upgrade to angular 11

--- src/app/pages/blog-root/blog-root.component.spec.ts
+++ src/app/pages/blog-root/blog-root.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BlogRootComponent } from './blog-root.component';
 
@@ -6,7 +6,7 @@ describe('BlogRootComponent', () => {
   let component: BlogRootComponent;
   let fixture: ComponentFixture<BlogRootComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BlogRootComponent ]
     })
